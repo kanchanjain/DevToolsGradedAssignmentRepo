9c5310acb7b1f13465765119849ec6d08bd702b8
student <kmkanchan.jain@hcl.com>
2021-04-21 02:00:23 +0000

    first commit

---
+++ src/main/java/com/kanchan/DevTolsGradedAssignment/DevTolsGradedAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.kanchan.DevTolsGradedAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTolsGradedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTolsGradedAssignmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ src/test/java/com/kanchan/DevTolsGradedAssignment/DevTolsGradedAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanchan.DevTolsGradedAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTolsGradedAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
